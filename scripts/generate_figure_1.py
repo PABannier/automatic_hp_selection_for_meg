c0a3ad1eb3a9c643dc870ae541e5775bbd985800
Pierre-Antoine Bannier <pierre-antoine@mbpdepieantoine.home>
2021-08-31 17:28:02 +0200

    pass on metrics

--- scripts/generate_figure_1.py
+++ scripts/generate_figure_1.py
@@ -17,8 +17,69 @@ from hp_selection.utils import apply_solver
 from hp_selection.utils import load_data
 
 CONDITION = "auditory/left"
-amplitude_range = [(30 + 25 * i, 30 + 25 * j) for i, j in
-                   zip(range(0, 10), range(0, 10))]
+# AMPLITUDE_RANGE = [(30 + 25 * i, 30 + 25 * j) for i, j in
+#                    zip(range(0, 10), range(0, 10))]
+
+AMPLITUDE_RANGE = [(120, 120)]
+# SOLVERS = ["temporal_cv", "spatial_cv", "lambda_map", "sure"]
+SOLVERS = ["spatial_cv"]
+
+def delta_f1_score(stc, true_stc, forward, subject, labels, extent,
+                   subjects_dir):
+    vertices = []
+    vertices.append(forward["src"][0]["vertno"])
+    vertices.append(forward["src"][1]["vertno"])
+
+    # Reverse the effect of make_sparse_stc
+    # stc now contains a sparse matrix with a lot of zero rows and few non-zero
+    # rows containing the activations
+
+    stc.expand(vertices)
+    true_stc.expand(vertices)
+
+    estimated_activations = np.abs(stc.data).sum(axis=-1)
+    true_activations = np.abs(true_stc.data).sum(axis=-1)
+
+    estimated_as = estimated_activations != 0
+    true_as = true_activations != 0
+
+    map_hemis = {"lh": 0, "rh": 1}
+
+    # XXX: We assume one source per hemisphere
+    ext_vertices_lh = mne.label.grow_labels(
+        subject, labels[0].vertices, extent, map_hemis[labels[0].hemi],
+        subjects_dir=subjects_dir)[0].vertices
+    ext_vertices_rh = mne.label.grow_labels(
+        subject, labels[1].vertices, extent, map_hemis[labels[1].hemi],
+        subjects_dir=subjects_dir)[0].vertices
+
+    ext_vertices_lh = np.intersect1d(ext_vertices_lh,
+                                     forward["src"][0]["vertno"])
+    ext_vertices_rh = np.intersect1d(ext_vertices_rh,
+                                     forward["src"][1]["vertno"])
+
+    ext_indices_lh = np.searchsorted(forward["src"][0]["vertno"],
+                                     ext_vertices_lh)
+    ext_indices_rh = np.searchsorted(forward["src"][1]["vertno"],
+                                     ext_vertices_rh)
+
+    ext_pred_lh = np.zeros(len(forward["src"][0]["vertno"]), dtype=int)
+    ext_pred_lh[ext_indices_lh] = 1
+    ext_pred_rh = np.zeros(len(forward["src"][1]["vertno"]), dtype=int)
+    ext_pred_rh[ext_indices_rh] = 1
+
+    extended_true_as = np.concatenate((ext_pred_lh, ext_pred_rh))
+
+    delta_precision = precision_score(extended_true_as, estimated_as,
+                                      zero_division=0)
+    recall = recall_score(true_as, estimated_as, zero_division=0)
+
+    if delta_precision == 0 and recall == 0:
+        return 0, delta_precision, recall
+    f1 = 2 * (delta_precision * recall) / (delta_precision + recall)
+
+    return f1, delta_precision, recall
+
 
 if __name__ == "__main__":
     simulated = True
@@ -30,22 +91,23 @@ if __name__ == "__main__":
     dict_precision = {}
     dict_delta_precision = {}
 
-    extent = 10
+    extent = 20
 
     subject = "sample"
     subjects_dir = mne.datasets.sample.data_path() + '/subjects'
 
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, sharey=True)
 
-    for solver in ["temporal_cv", "spatial_cv", "lambda_map", "sure"]:
+    for solver in SOLVERS:
         f1_scores = list()
         delta_precision_scores = list()
         recall_scores = list()
 
-        for amplitude in amplitude_range:
+        for amplitude in AMPLITUDE_RANGE:
             evoked, forward, noise_cov, true_stc, labels = load_data(
                 CONDITION, maxfilter=maxfilter, simulated=simulated,
-                amplitude=amplitude, return_stc=True, return_labels=True)
+                amplitude=amplitude, return_stc=True, return_labels=True, 
+                resolution=6)
 
             if solver == "sure":
                 stc = solve_using_sure(evoked, forward, noise_cov, depth=0.99)
@@ -61,62 +123,14 @@ if __name__ == "__main__":
             else:
                 raise ValueError("Unknown solver!")
 
-            vertices = []
-            vertices.append(forward["src"][0]["vertno"])
-            vertices.append(forward["src"][1]["vertno"])
+            f1, delta_precision, recall = delta_f1_score(stc, true_stc, forward,
+                subject, labels, extent, subjects_dir)
 
-            stc.expand(vertices)
-            true_stc.expand(vertices)
-
-            estimated_active_set = np.abs(stc.data).sum(axis=-1)
-            true_active_set = np.abs(true_stc.data).sum(axis=-1)
-
-            # estimated_active_set /= estimated_active_set.sum()
-            # true_active_set /= true_active_set.sum()
-
-            # Compute delta-precision
-            support_bin = true_active_set != 0
-            normalized_score = estimated_active_set
-
-            map_hemis = {"lh": 0, "rh": 1}
-
-            # XXX: We assume one source per hemisphere
-            ext_vertices_lh = mne.label.grow_labels(
-                subject, labels[0].vertices, extent, map_hemis[labels[0].hemi],
-                subjects_dir=subjects_dir)[0].vertices
-            ext_vertices_rh = mne.label.grow_labels(
-                subject, labels[1].vertices, extent, map_hemis[labels[1].hemi],
-                subjects_dir=subjects_dir)[0].vertices
-
-            ext_vertices_lh = np.intersect1d(ext_vertices_lh,
-                                             forward["src"][0]["vertno"])
-            ext_vertices_rh = np.intersect1d(ext_vertices_rh,
-                                             forward["src"][1]["vertno"])
-
-            ext_indices_lh = np.searchsorted(forward["src"][0]["vertno"],
-                                             ext_vertices_lh)
-            ext_indices_rh = np.searchsorted(forward["src"][1]["vertno"],
-                                             ext_vertices_rh)
-
-            ext_pred_lh = np.zeros(len(forward["src"][0]["vertno"]), dtype=int)
-            ext_pred_lh[ext_indices_lh] = 1
-            ext_pred_rh = np.zeros(len(forward["src"][1]["vertno"]), dtype=int)
-            ext_pred_rh[ext_indices_rh] = 1
-
-            support_ext_bin = np.concatenate((ext_pred_lh, ext_pred_rh))
-            # support_ext_bin = support_ext_bin.astype(int)
-
-            delta_precision = precision_score(support_ext_bin,
-                                              normalized_score != 0,
-                                              zero_division=1)
-            recall = recall_score(support_bin, normalized_score != 0)
-
-            f1 = 2 * (delta_precision * recall) / (delta_precision + recall)
             delta_precision_scores.append(delta_precision)
             recall_scores.append(recall)
             f1_scores.append(f1)
 
-        amplitudes = [x[0] for x in amplitude_range]
+        amplitudes = [x[0] for x in AMPLITUDE_RANGE]
         ax1.plot(amplitudes, delta_precision_scores, label=solver)
         ax2.plot(amplitudes, recall_scores, label=solver)
         ax3.plot(amplitudes, f1_scores, label=solver)
